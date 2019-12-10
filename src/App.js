7ffe34d14a5b566f300b022fa0c339fd3e82c21b
Lekshmi A K <lekshmi@Lekshmis-MacBook-Air.local>
2019-12-10 17:37:14 +0530

    code change

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { TOKEN } from "./token";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  handleStatusChange = () => {
+    let userInput = document.getElementById("idInput").value;
+    let endPoint = "https://mern-app-sample-1.herokuapp.com";
+    let token = TOKEN;
+    console.log(token);
+    let url = endPoint + "/changeUserPlivoEnabledState?";
+    fetch(url, {
+      method: "put",
+      headers: {
+        // "Content-Type": "application/json",
+        "x-auth-id-token": token
+      },
+      body: JSON.stringify({
+        isPlivoEnabled: false,
+        firebaseId: "UldvXHBwuKZYVyT4enqVD2PpFvN2"
+      })
+    })
+      .then(res => {
+        this.Auth.handleError(res.status);
+        return res.json();
+      })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+  render() {
+    return (
+      <>
+        <div style={{ textAlign: "center", display: "flex" }}>
+          <h3>Change status</h3>
+          <input id="idInput"></input>
+          <button onClick={this.handleStatusChange}>Set</button>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
