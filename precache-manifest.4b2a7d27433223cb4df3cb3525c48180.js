dfea7dfb8f8b4e94a575b4c4e16ae8fe80d339d7
Lekshmi A K <lekshmi@Lekshmis-MacBook-Air.local>
2019-12-10 18:24:23 +0530

    Updates

---
+++ precache-manifest.4b2a7d27433223cb4df3cb3525c48180.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ed920e862dbb8e5aab89296bf373c6",
+    "url": "/testReact/index.html"
+  },
+  {
+    "revision": "6dd3e7adca92e1919ea9",
+    "url": "/testReact/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3e263a233a555456718e",
+    "url": "/testReact/static/js/2.74d9b245.chunk.js"
+  },
+  {
+    "revision": "6dd3e7adca92e1919ea9",
+    "url": "/testReact/static/js/main.8f621233.chunk.js"
+  },
+  {
+    "revision": "dec2c23031ba8aa7da34",
+    "url": "/testReact/static/js/runtime-main.a6d77ace.js"
+  }
+]);
